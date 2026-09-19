ddfddb393feafd5f1b521774d024e588ad3f1de6
Redwan Ahmed <redwanahmedridoy2005@gmail.com>
2026-09-19 16:11:41 +0600

    HackerRank Nested Lists
    HackerRank Nested Lists

---
+++ nested-list.py
@@ -0,0 +1,35 @@
+# Proglem Link : https://www.hackerrank.com/challenges/nested-list/problem?isFullScreen=true
+
+
+def find_second_lowest_grade(records):
+    scores = []
+    names = []
+
+    for record in records:
+        scores.append(record[1])
+
+    sorted_score = sorted(set(scores))
+
+    second_lowest_score = sorted_score[1]
+
+    for score in records:
+        if score[1] == second_lowest_score:
+            names.append(score[0])
+
+    names = sorted(names)
+
+    for name in names:
+        print(name)
+
+
+if __name__ == '__main__':
+    records = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+        records.append([name,score])
+
+
+    find_second_lowest_grade(records)
+
